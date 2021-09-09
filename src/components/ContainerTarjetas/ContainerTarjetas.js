088c8247569d278e8c36ca4e9db6bff626664ff3
Victoria <vickyduque12@gmail.com>
2021-09-09 09:40:58 -0300

    agregamos eliminar y ver mas

--- src/components/ContainerTarjetas/ContainerTarjetas.js
+++ src/components/ContainerTarjetas/ContainerTarjetas.js
@@ -5,35 +5,55 @@ export default class ContainerTarjetas extends Component {
     constructor(props) {
         super(props);
         this.state = {
-            tarjetas: []
+            tarjetas: [ ], // array de todas las pelis 
+            filteredMovies: [ ] // pelis filtradas para eliminar 
         }
     }
 
+    // Pasamos la ulr de la API. 
+    // Siempre el llamado a apis tiene que estar en un container y ese container hace el llamado despues a cada una de las tarjetas individualmente.
     componentDidMount() {
         fetch('https://api.themoviedb.org/3/movie/popular?api_key=274e06a482f13c5a152ff7abe7a3142a&language=en-US&page=1')
             .then(response => { return response.json() })
             .then(data => {
-               
-
-                //A la información que obtengo la guardo en el estado dentro de una propiedad
+            //Lo que traemos del fetch se almacena en el estado (con setState) para luego ser accedido.
+            //Con this.state estamos llamando a ese estado, y dentro esta la propiedad "tarjetas" que contiene la informacion a mappear
+            //A la información que obtengo la guardo en el estado dentro de una propiedad
                 this.setState({
-                    tarjetas: data.results
+                    tarjetas: data.results  // Results porque es la palabra que te da la api para devolverte los resultados de busqueda.
                 })
             })
             .catch(error => console.log(error));
     }
+
+
+     //Metodo que se llama cuando se hace clic en eliminar pelicula. 
+     removerPelicula(title){      //Obtengo el titulo de la peli que quiero borrar del array. 
+        //Quiero filtrar todas las peliculas que  no tengan ese nombre. Permancen en el array aquellas peliculas que no tengan el nombre a filtrar 
+       const peliculasFiltradas = this.state.tarjetas.filter(tarjeta => tarjeta.title !== title) //!== Significa distinto. 
+
+       //Ultimo paso es setear el estado, asi no queda en la nada:
+       this.setState({
+           filteredMovies: peliculasFiltradas,
+           tarjetas: peliculasFiltradas  //se vuelve a renderizar en pantalla. 
+       })
+    }
+
+
     render() {
         return (
             <div>
                 {this.state.tarjetas.map((tarjeta, index) => {
                         return <Tarjetas key={index}
                         title={tarjeta.title}
-                        backdrop_path={tarjeta.backdrop_path}
+                        backdrop_path={tarjeta.poster_path}
                         overview = {tarjeta.overview}
-                        // removerPersonaje = {(name)=>this.removerPersonaje(name)}
+                        release_date =  {tarjeta.release_date}
+                        removerPelicula = {(title) => this.removerPelicula(title)} //paso una funcion mediante props para que elimine la peli. Recibe el id de cada peli. 
                         />
                     })
                 }
+        
             </div>
         )
     }
--- src/components/Tarjetas/Tarjetas.js
+++ src/components/Tarjetas/Tarjetas.js
@@ -3,18 +3,44 @@ import React, { Component } from 'react';
 //Componente de presentación (sin lógica)
 export default class Tarjetas extends Component {
     constructor(props){
-        super(props)
+        super(props);
+        this.state = {
+            clase: "none", //La idea es que no se muestre la descripcion al prinicpio. 
+             mensaje: "Ver Más",
+        }
+    
     }
+
+     //Metodo que se llama cuando se hace clic en ver mas. 
+     handleShow(){
+            if (this.state.clase === "none") { //si esta en hide, la idea es que se cambie a show. 
+                this.setState({
+                    clase: "block",
+                    mensaje: "Ver Menos"
+                })
+                } else {
+                    this.setState({   //si no esta en hide, esta en show. SI esta en show, al hacerle clic la idea es que se esconda. 
+                        clase: "none",
+                        mensaje: "Ver Mas"
+                    })
+            }
+        }
+
+       
 
     render() {
         return (
             <div>
                <h4> {this.props.title} </h4>
-               <img alt='20' src={`https://image.tmdb.org/t/p/w500/${this.props.backdrop_path}`}></img>
-               <h4> Descripcion: {this.props.overview}</h4>
-               <button>Ver mas</button>
-               <button onClick={() => this.handleShow()}>Ver más</button>
-               {/* <button onClick={()=> this.props.removerPersonaje(this.props.name)}> Eliminar pelicula</button> */}
+               <h4> Fecha de estreno: {this.props.release_date} </h4>
+               <img alt='20' src={`https://image.tmdb.org/t/p/w342/${this.props.backdrop_path}`}></img>
+               
+                <br/>
+                <h4 style={{display: this.state.clase }}> {this.props.overview} </h4>
+               <button onClick={() => this.handleShow(this.props.overview)}> {this.state.mensaje} </button>  {/* agregamos evento onclick  */}
+
+               <button onClick={()=> this.props.removerPelicula(this.props.title)}> Eliminar pelicula </button>
+           
             </div>
         )
     }
